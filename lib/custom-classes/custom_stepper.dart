aeb05028a00ebedf86aaa36b79c850eb2f947dd6
Rasel Ahmed <rasel@augnitive.com>
2019-12-10 10:25:03 +0600

    CA-11 Remove commented code

--- lib/custom-classes/custom_stepper.dart
+++ lib/custom-classes/custom_stepper.dart
@@ -1,46 +1,17 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/widgets.dart';
 
 import 'package:flutter/material.dart';
 
-// TODO(dragostis): Missing functionality:
-//   * mobile horizontal mode with adding/removing steps
-//   * alternative labeling
-//   * stepper feedback in the case of high-latency interactions
-
-/// The state of a [Step] which is used to control the style of the circle and
-/// text.
-///
-/// See also:
-///
-///  * [Step]
 enum CustomStepState {
-  /// A step that displays its index in its circle.
   indexed,
-
-  /// A step that displays a pencil icon in its circle.
   editing,
-
-  /// A step that displays a tick icon in its circle.
   complete,
-
-  /// A step that is disabled and does not to react to taps.
   disabled,
-
-  /// A step that is currently having an error. e.g. the user has submitted wrong
-  /// input.
   error,
 }
 
-/// Defines the [Stepper]'s main axis.
 enum CustomStepperType {
-  /// A vertical layout of the steps with their content in-between the titles.
   vertical,
-
-  /// A horizontal layout of the steps with their content below the titles.
   horizontal,
 }
 
@@ -57,19 +28,9 @@ const Color _kDisabledDark = Colors.white38;
 const double _kStepSize = 24.0;
 const double _kTriangleHeight = _kStepSize * 0.866025; // Triangle height. sqrt(3.0) / 2.0
 
-/// A material step used in [Stepper]. The step can have a title and subtitle,
-/// an icon within its circle, some content and a state that governs its
-/// styling.
-///
-/// See also:
-///
-///  * [Stepper]
-///  * <https://material.io/archive/guidelines/components/steppers.html>
 @immutable
 class CustomStep {
-  /// Creates a step for a [Stepper].
-  ///
-  /// The [title], [content], and [state] arguments must not be null.
+  
   const CustomStep({
     @required this.title,
     this.subtitle,
@@ -80,49 +41,15 @@ class CustomStep {
        assert(content != null),
        assert(state != null);
 
-  /// The title of the step that typically describes it.
   final Widget title;
-
-  /// The subtitle of the step that appears below the title and has a smaller
-  /// font size. It typically gives more details that complement the title.
-  ///
-  /// If null, the subtitle is not shown.
   final Widget subtitle;
-
-  /// The content of the step that appears below the [title] and [subtitle].
-  ///
-  /// Below the content, every step has a 'continue' and 'cancel' button.
   final Widget content;
-
-  /// The state of the step which determines the styling of its components
-  /// and whether steps are interactive.
   final CustomStepState state;
-
-  /// Whether or not the step is active. The flag only influences styling.
   final bool isActive;
 }
 
-/// A material stepper widget that displays progress through a sequence of
-/// steps. Steppers are particularly useful in the case of forms where one step
-/// requires the completion of another one, or where multiple steps need to be
-/// completed in order to submit the whole form.
-///
-/// The widget is a flexible wrapper. A parent class should pass [currentStep]
-/// to this widget based on some logic triggered by the three callbacks that it
-/// provides.
-///
-/// See also:
-///
-///  * [Step]
-///  * <https://material.io/archive/guidelines/components/steppers.html>
 class CustomStepper extends StatefulWidget {
-  /// Creates a stepper from a list of steps.
-  ///
-  /// This widget is not meant to be rebuilt with a different list of steps
-  /// unless a key is provided in order to distinguish the old stepper from the
-  /// new one.
-  ///
-  /// The [steps], [type], and [currentStep] arguments must not be null.
+  
   const CustomStepper({
     Key key,
     @required this.steps,
@@ -139,91 +66,13 @@ class CustomStepper extends StatefulWidget {
        assert(0 <= currentStep && currentStep < steps.length),
        super(key: key);
 
-  /// The steps of the stepper whose titles, subtitles, icons always get shown.
-  ///
-  /// The length of [steps] must not change.
   final List<CustomStep> steps;
-
-  /// How the stepper's scroll view should respond to user input.
-  ///
-  /// For example, determines how the scroll view continues to
-  /// animate after the user stops dragging the scroll view.
-  ///
-  /// If the stepper is contained within another scrollable it
-  /// can be helpful to set this property to [ClampingScrollPhysics].
   final ScrollPhysics physics;
-
-  /// The type of stepper that determines the layout. In the case of
-  /// [StepperType.horizontal], the content of the current step is displayed
-  /// underneath as opposed to the [StepperType.vertical] case where it is
-  /// displayed in-between.
   final CustomStepperType type;
-
-  /// The index into [steps] of the current step whose content is displayed.
   final int currentStep;
-
-  /// The callback called when a step is tapped, with its index passed as
-  /// an argument.
   final ValueChanged<int> onStepTapped;
-
-  /// The callback called when the 'continue' button is tapped.
-  ///
-  /// If null, the 'continue' button will be disabled.
   final VoidCallback onStepContinue;
-
-  /// The callback called when the 'cancel' button is tapped.
-  ///
-  /// If null, the 'cancel' button will be disabled.
   final VoidCallback onStepCancel;
-
-  /// The callback for creating custom controls.
-  ///
-  /// If null, the default controls from the current theme will be used.
-  ///
-  /// This callback which takes in a context and two functions,[onStepContinue]
-  /// and [onStepCancel]. These can be used to control the stepper.
-  ///
-  /// {@tool snippet --template=stateless_widget_scaffold}
-  /// Creates a stepper control with custom buttons.
-  ///
-  /// ```dart
-  /// Widget build(BuildContext context) {
-  ///   return Stepper(
-  ///     controlsBuilder:
-  ///       (BuildContext context, {VoidCallback onStepContinue, VoidCallback onStepCancel}) {
-  ///          return Row(
-  ///            children: <Widget>[
-  ///              FlatButton(
-  ///                onPressed: onStepContinue,
-  ///                child: const Text('CONTINUE'),
-  ///              ),
-  ///              FlatButton(
-  ///                onPressed: onStepCancel,
-  ///                child: const Text('CANCEL'),
-  ///              ),
-  ///            ],
-  ///          );
-  ///       },
-  ///     steps: const <Step>[
-  ///       Step(
-  ///         title: Text('A'),
-  ///         content: SizedBox(
-  ///           width: 100.0,
-  ///           height: 100.0,
-  ///         ),
-  ///       ),
-  ///       Step(
-  ///         title: Text('B'),
-  ///         content: SizedBox(
-  ///           width: 100.0,
-  ///           height: 100.0,
-  ///         ),
-  ///       ),
-  ///     ],
-  ///   );
-  /// }
-  /// ```
-  /// {@end-tool}
   final ControlsWidgetBuilder controlsBuilder;
 
   @override
@@ -695,8 +544,6 @@ class _StepperState extends State<CustomStepper> with TickerProviderStateMixin {
   }
 }
 
-// Paints a triangle whose base is the bottom of the bounding rectangle and its
-// top vertex the middle of its top.
 class _TrianglePainter extends CustomPainter {
   _TrianglePainter({
     this.color,
