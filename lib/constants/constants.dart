969658764e00a2a00b246a630b5ff755f1b05424
Rasel Ahmed <rasel@augnitive.com>
2020-12-20 20:02:59 +0600

    New url added

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 //urls
-const String apiUrl = 'http://84abd792dcc1.ngrok.io/api/v1/';
+// const String apiUrl = 'http://84abd792dcc1.ngrok.io/api/v1/';
 //const String apiUrl = 'https://coordinate-api.monarko.com/api/v1/';
-// const String apiUrl = 'https://brac-api.ghrucoordinate.com/api/v1/';
+const String apiUrl = 'https://brac-api.ghrucoordinate.com/api/v1/';
 
 const String gsBucket = 'gs://brac-coordinate.appspot.com';
 
