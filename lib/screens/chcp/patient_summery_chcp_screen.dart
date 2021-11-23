09d57ccc96bd8f12f98f3927f409103eacbe40f9
NafizZZ <nafiz@augnitive.com>
2021-11-23 19:17:11 +0600

    getMedicationsConditions() local

--- lib/screens/chcp/patient_summery_chcp_screen.dart
+++ lib/screens/chcp/patient_summery_chcp_screen.dart
@@ -296,12 +296,8 @@ class _PatientSummeryChcpScreenState extends State<PatientSummeryChcpScreen> {
   }
 
   getMedicationsConditions() async {
-    isLoading = true;
-    if (Auth().isExpired()) {
-      Auth().logout();
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (ctx) => AuthScreen()));
-    }
-    var fetchedSurveys = await ObservationController().getLiveSurveysByPatient();
+    var patientId = Patient().getPatient()['id'];
+    var fetchedSurveys = await ObservationController().getLocalSurveysByPatient(patientId);
 
     if(fetchedSurveys.isNotEmpty) {
       fetchedSurveys.forEach((item) {
