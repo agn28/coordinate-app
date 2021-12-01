be864eb93b4290f16fc091f4e7632637ff98b662
Nafiz <nafiz@augnitive.com>
2021-12-01 19:22:45 +0600

    community clinic followup date added in chw referral screen, full_assessment_chcp_screen, new_patient_questionnaire_chcp_screen, short_followup_chcp_screen

--- lib/screens/chcp/full_assessment_chcp_screen.dart
+++ lib/screens/chcp/full_assessment_chcp_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:basic_utils/basic_utils.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -82,7 +83,7 @@ var clinicNameController = TextEditingController();
 
 var clinicTypes = [];
 var _patient;
-
+var nextVisitDateController = TextEditingController();
 bool refer = false;
 
 getName(context, item) {
@@ -278,6 +279,7 @@ class _FullAssessmentChcpScreenState extends State<FullAssessmentChcpScreen> {
     selectedReason = null;
     selectedtype = null;
     clinicNameController.text = '';
+    nextVisitDateController.text = '';
   }
 
   _checkAuth() {
@@ -933,6 +935,7 @@ class _FullAssessmentChcpScreenState extends State<FullAssessmentChcpScreen> {
           'clinic_type' : selectedtype,
           'clinic_name' : clinicNameController.text,
         },
+        'follow_up_date': nextVisitDateController.text,
       },
       'referred_from': 'community clinic',
     };
@@ -5479,6 +5482,7 @@ class _CreateReferState extends State<CreateRefer> {
     referralReasons = referralReasonOptions['options']; 
     referralToRoles = referralToRolesOptions['options']; 
     // print('encounterData $encounterData');
+    nextVisitDateController.text = '${DateFormat("yyyy-MM-dd").format(DateTime.now())}';
   }
 
   // _getAuthData() async {
@@ -5663,6 +5667,44 @@ class _CreateReferState extends State<CreateRefer> {
                               hintStyle: TextStyle(fontSize: 18)
                             ),
                           )
+                        ),
+                        SizedBox(height: 30,),
+                        
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(AppLocalizations.of(context).translate("followupIn"), style: TextStyle(color: Colors.black, fontSize: 20)),
+                        ),
+                        SizedBox(height: 10,),
+
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 25),
+                          child: DateTimeField(
+                            resetIcon: null,
+                            format: DateFormat("yyyy-MM-dd"),
+                            controller: nextVisitDateController,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.black45, fontSize: 19.0),
+                              contentPadding: EdgeInsets.only(top: 18, bottom: 18),
+                              prefixIcon: Icon(Icons.date_range),
+                              filled: true,
+                              fillColor: kSecondaryTextField,
+                              border: new UnderlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(4),
+                                  topRight: Radius.circular(4),
+                                )
+                              ),
+                            ),
+                            
+                            onShowPicker: (context, currentValue) async  {
+                              return showDatePicker(
+                                  context: context,
+                                  firstDate: DateTime(1900),
+                                  initialDate: currentValue ?? DateTime.now(),
+                                  lastDate: DateTime(2100));
+                            },
+                          ),
                         ),
 
                         SizedBox(height: 50,),
--- lib/screens/chcp/short_followup_chcp_screen.dart
+++ lib/screens/chcp/short_followup_chcp_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:basic_utils/basic_utils.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -77,7 +78,7 @@ var selectedtype;
 var clinicNameController = TextEditingController();
 
 var _patient;
-
+var nextVisitDateController = TextEditingController();
 bool refer = false;
 
 getQuestionText(context, question) {
@@ -356,6 +357,7 @@ class _FollowupVisitChcpScreenState extends State<FollowupVisitChcpScreen> {
     selectedReason = null;
     selectedtype = null;
     clinicNameController.text = '';
+    nextVisitDateController.text = '';
   }
 
   _checkAuth() {
@@ -930,6 +932,7 @@ var role = '';
           'clinic_type' : selectedtype,
           'clinic_name' : clinicNameController.text,
         },
+        'follow_up_date': nextVisitDateController.text,
       },
       'referred_from': 'new questionnaire chcp',
     };
@@ -3952,6 +3955,7 @@ class _CreateReferState extends State<CreateRefer> {
     referralReasons = referralReasonOptions['options']; 
     referralToRoles = referralToRolesOptions['options']; 
     // print('encounterData $encounterData');
+    nextVisitDateController.text = '${DateFormat("yyyy-MM-dd").format(DateTime.now())}';
   }
 
   // _getAuthData() async {
@@ -4158,6 +4162,44 @@ class _CreateReferState extends State<CreateRefer> {
                               hintStyle: TextStyle(fontSize: 18)
                             ),
                           )
+                        ),
+                        SizedBox(height: 30,),
+                        
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(AppLocalizations.of(context).translate("followupIn"), style: TextStyle(color: Colors.black, fontSize: 20)),
+                        ),
+                        SizedBox(height: 10,),
+
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 25),
+                          child: DateTimeField(
+                            resetIcon: null,
+                            format: DateFormat("yyyy-MM-dd"),
+                            controller: nextVisitDateController,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.black45, fontSize: 19.0),
+                              contentPadding: EdgeInsets.only(top: 18, bottom: 18),
+                              prefixIcon: Icon(Icons.date_range),
+                              filled: true,
+                              fillColor: kSecondaryTextField,
+                              border: new UnderlineInputBorder(
+                                borderSide: new BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(4),
+                                  topRight: Radius.circular(4),
+                                )
+                              ),
+                            ),
+                            
+                            onShowPicker: (context, currentValue) async  {
+                              return showDatePicker(
+                                  context: context,
+                                  firstDate: DateTime(1900),
+                                  initialDate: currentValue ?? DateTime.now(),
+                                  lastDate: DateTime(2100));
+                            },
+                          ),
                         ),
                         SizedBox(height: 50,),
                       ],
